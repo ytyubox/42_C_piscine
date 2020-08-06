2cb953f58843b36e37f6942f9d00ca889cad128b
Tsungyu Yu <ytyubox@gmail.com>
2020-08-06 16:03:00 +0800

    add putchar

---
+++ 42_C_piscine/ft_putchar.c
@@ -0,0 +1,13 @@
+//
+//  ft_putchar.c
+//  42_C_piscine
+//
+//  Created by 游宗諭 on 2020/8/6.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+#include "ft_putchar.h"
+#include <unistd.h>
+void ft_putchar(char c) {
+    write(1,&c,1);
+}
---
+++ 42_C_piscine/ft_putchar.h
@@ -0,0 +1,14 @@
+//
+//  ft_putchar.h
+//  42_C_piscine
+//
+//  Created by 游宗諭 on 2020/8/6.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+#ifndef ft_putchar_h
+#define ft_putchar_h
+
+#include <stdio.h>
+void ft_putchar(char c);
+#endif /* ft_putchar_h */
