b1a9ffc82905d24239865107713e6fc35e3e8e40
Tsungyu Yu <ytyubox@gmail.com>
2020-08-06 15:58:45 +0800

    Initial Commit

---
+++ 42_C_piscine/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  42_C_piscine
+//
+//  Created by 游宗諭 on 2020/8/6.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
