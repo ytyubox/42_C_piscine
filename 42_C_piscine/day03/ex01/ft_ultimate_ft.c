bda7aab2e7672e79dc3e2718be84d476ed4e8ed1
Tsungyu Yu <ytyubox@gmail.com>
2020-08-11 21:49:27 +0800

    ft_ultimate_ft OK

---
+++ 42_C_piscine/day03/ex01/ft_ultimate_ft.c
@@ -0,0 +1,12 @@
+//
+//  ft_ultimate_ft.c
+//  42_C_piscine
+//
+//  Created by 游宗諭 on 2020/8/11.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+#include "ft_ultimate_ft.h"
+void ft_ultimate_ft(int *********nbr) {
+    *********nbr = 42;
+}
---
+++ 42_C_piscine/day03/ex01/ft_ultimate_ft.h
@@ -0,0 +1,14 @@
+//
+//  ft_ultimate_ft.h
+//  42_C_piscine
+//
+//  Created by 游宗諭 on 2020/8/11.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+#ifndef ft_ultimate_ft_h
+#define ft_ultimate_ft_h
+
+#include <stdio.h>
+ void ft_ultimate_ft(int *********nbr);
+#endif /* ft_ultimate_ft_h */
